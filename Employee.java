23541fd0ef103d3b42d0f15c7b5ced0222f9f7f0
Frank Roland Fernandes <frank1c53@gmail.com>
2024-02-14 21:19:50 +0530

    data didnt got updated
    updating the data

--- Employee.java
+++ Employee.java
@@ -1,3 +1,5 @@
+//find out what is the problem in the class 
+
 class Employee{
 	int id;
 	String name;
@@ -5,6 +7,24 @@ class Employee{
 		 this.id = id;
 		 this.name = name;
 	}
+	
+}
+class register{
+   Map<Employee, int> mapEmployee = new Hashmap<>();
+   mapEmployee.put(new Employee(1,"Ravi"),1);
+   mapEmployee.put(new Employee(1,"Ravi"),1);
+}
+
+//ans
+
+class Employee{
+	int id;
+	String name;
+	public Employee(int id, String name){
+		 this.id = id;
+		 this.name = name;
+	}
+	//I told her to add hashcode and equals to avoid duplicate entry. she told me to write equals and hashcode method so wrote code for equals
 	equals(Employee o){
 			
 	    return (this.o.id == o.id) && (this.name.equals(name));
@@ -13,6 +33,7 @@ class Employee{
 	}
 }
 class register{
+	//change int to integer
    Map<Employee, Integer> mapEmployee = new Hashmap<>();
    mapEmployee.put(new Employee(1,"Ravi"),1);
    mapEmployee.put(new Employee(1,"Ravi"),1);
